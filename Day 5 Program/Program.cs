8c8872dba334f18dcc032fee7635a5ce1bee3119
vimaley <Vimal.George1@gds.ey.com>
2023-02-20 07:21:28 +0000

    Add project files.

---
+++ Day 5 Program/Program.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day_5_Program
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            const float pi = 3.142f;
+//            float pi1 = 3.142f;
+//            Console.WriteLine("pi:" + pi);
+//            Console.WriteLine("pi1:" + pi1);
+
+//            Program obj = new Program();
+//            obj.function1();
+//            Program.function2();
+//            Type t1 = typeof(Program);
+//            Console.WriteLine(t1.Name);
+//            Console.WriteLine(t1.FullName);
+//            Console.ReadLine();
+//        }
+
+//        public void function1()
+//        {
+//            Console.WriteLine("Function 1 is invoked");
+//        }
+
+//        public static void function2()
+//        {
+//            Console.WriteLine("static void function2() is invoked");
+//        }
+
+//        static void function3()
+//        {
+//            Console.WriteLine("static void function3 is invoked");
+//        }
+//    }
+//}
---
+++ Day 5 Program/issopp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Program
+{
+    class myclass1 { }
+
+    class myclass2 { }
+
+    class myclass
+    {
+        static void Main(string[] args)
+        {
+            myclass1 myclass1 = new myclass1();
+            myclass2 myclass2 = new myclass2();
+
+            test(myclass1);
+            test(myclass2);
+
+            Console.ReadLine();
+        }
+
+        static void test(object obj)
+        {
+            myclass1 obj1;
+            myclass2 obj2;
+
+            if(obj is myclass1)
+            {
+                Console.WriteLine("obj is myclass1");
+                obj1 = (myclass1)obj;
+
+            }
+            else if(obj is myclass2)
+            {
+                Console.WriteLine("obj is myclass2");
+                obj2 = (myclass2)obj;
+            }
+            else
+            {
+                Console.WriteLine("obj is neither class1 and neither class2");
+            }
+        }
+    }
+    
+}
